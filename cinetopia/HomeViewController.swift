b07ec04c6b476be5aac60221717a2932b4aa7537
João Alexandre Bitar <joaoalexandre.bitar@gmail.com>
2023-12-15 13:22:38 -0300

    chore: initial setup

---
+++ cinetopia/HomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  cinetopia
+//
+//  Created by João Alexandre Bitar on 12/12/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+    // MARK: - Helpers
+    
+    
+}
+
